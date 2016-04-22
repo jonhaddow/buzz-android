e2eacb0498e9ddc7d7d79d6f8551d4501b263456
jonhaddow <haddowjon@gmail.com>
2016-04-22 13:00:46 +0100

    Tidy time remaining bar

--- app/src/main/java/com/jon/buzz/Activities/MainActivity.java
+++ app/src/main/java/com/jon/buzz/Activities/MainActivity.java
@@ -1,5 +1,6 @@
-package com.jon.buzz.Activities;
+package com.jon.buzz.activities;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -13,31 +14,64 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.jon.buzz.Adapters.MyPagerAdapter;
-import com.jon.buzz.Interfaces.StartTimerListener;
 import com.jon.buzz.R;
-import com.jon.buzz.Services.BackgroundCountdown;
+import com.jon.buzz.adapters.MyPagerAdapter;
+import com.jon.buzz.interfaces.StartTimerListener;
+import com.jon.buzz.services.BackgroundCountdown;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements StartTimerListener, View.OnClickListener {
 
-	public static final String STOP_COUNTDOWN = "STOP_COUNTDOWN";
+	public static final String STOP_TIMER = "com.jon.buzz.activities.MainActivity.STOP_TIMER";
 	public ArrayList<String> mTimers = new ArrayList<>();
 	public ArrayAdapter<String> mListAdapter;
-	ViewPager mPager;
+
 	private TextView mTvTimeRemaining;
-	private ImageView mIvStopTimer;
 	private BroadcastReceiver receiver;
 	private int mSeconds;
+	private LocalBroadcastManager broadcastManager;
 
 	@Override
-	protected void onStart() {
+	protected void onPause() {
 
-		LocalBroadcastManager.getInstance(this).registerReceiver((receiver),
-				new IntentFilter(BackgroundCountdown.SECONDS_REMAINING)
-		);
-		super.onStart();
+		// Unregister receiver
+		broadcastManager.unregisterReceiver(receiver);
+
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+
+		// When broadcast is received, update time remaining
+		receiver = new BroadcastReceiver() {
+			@Override
+			public void onReceive(Context context, Intent intent) {
+
+				if (intent != null) {
+					updateTimeRemaining(intent.getIntExtra(BackgroundCountdown.SECONDS_REMAINING, 0));
+				}
+			}
+		};
+
+		// Register receiver
+		broadcastManager = LocalBroadcastManager.getInstance(this);
+		broadcastManager.registerReceiver((receiver),
+				new IntentFilter(BackgroundCountdown.TIME_REMAINING));
+
+		super.onResume();
+	}
+
+	private void updateTimeRemaining(int timeRemaining) {
+
+		String textToDisplay;
+		if (timeRemaining != 0) {
+			textToDisplay = "Time remaining... " + timeRemaining + " seconds";
+		} else {
+			textToDisplay = mSeconds + " second timer complete!";
+		}
+		mTvTimeRemaining.setText(textToDisplay);
 	}
 
 	@Override
@@ -46,58 +80,49 @@ public class MainActivity extends AppCompatActivity implements StartTimerListene
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		receiver = new BroadcastReceiver() {
-			@Override
-			public void onReceive(Context context, Intent intent) {
-
-				if (intent != null) {
-					mSeconds = intent.getIntExtra(BackgroundCountdown.SECONDS_REMAINING, 0);
-					mTvTimeRemaining.setText(String.valueOf(mSeconds));
-				}
-
-			}
-		};
 
 		mTvTimeRemaining = (TextView) findViewById(R.id.tv_time_remaining);
-		mIvStopTimer = (ImageView) findViewById(R.id.iv_stop_timer);
+		ImageView mIvStopTimer = (ImageView) findViewById(R.id.iv_stop_timer);
 		if (mIvStopTimer != null) {
 			mIvStopTimer.setOnClickListener(this);
 		}
 
 
 		// Instantiate view pager and pager adapter
-		mPager = (ViewPager) findViewById(R.id.pager);
+		ViewPager mPager = (ViewPager) findViewById(R.id.pager);
 		MyPagerAdapter mPagerAdapter = new MyPagerAdapter(getSupportFragmentManager(), this);
 		if (mPager != null) {
 			mPager.setAdapter(mPagerAdapter);
 		}
 	}
 
-	@Override
-	protected void onStop() {
-
-		LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
-		super.onStop();
-	}
-
 	/**
 	 * This method is called from set timer to add a timer to the list adapter on timer list fragment
 	 *
-	 * @param seconds
+	 * @param seconds number of seconds to set timer for
 	 */
 	@Override
 	public void startTimer(int seconds) {
+
+		// Save current timer seconds
+		mSeconds = seconds;
 
 		// Start a new countdown service
 		Intent countdownIntent = new Intent(this, BackgroundCountdown.class);
 		countdownIntent.putExtra("Seconds", seconds);
 		startService(countdownIntent);
-
-
 	}
 
 	@Override
 	public void onClick(View view) {
 
+		// Cancel all notifications
+		((NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE)).cancelAll();
+
+		// Clear time remaining text on bottom bar
+		mTvTimeRemaining.setText("");
+
+		// Stop Timer service
+		broadcastManager.sendBroadcast(new Intent(STOP_TIMER));
 	}
 }
