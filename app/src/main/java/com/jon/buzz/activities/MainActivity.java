aec467f090999a8df32094c3a5eebbe875952ee8
jonhaddow <haddowjon@gmail.com>
2016-05-24 15:21:10 +0100

    Created Settings Menu

--- app/src/main/java/com/jon/buzz/activities/MainActivity.java
+++ app/src/main/java/com/jon/buzz/activities/MainActivity.java
@@ -13,7 +13,9 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -187,6 +189,13 @@ public class MainActivity extends AppCompatActivity implements StartNewTimerList
 
 		getMenuInflater().inflate(R.menu.main_activity_menu, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		startActivity(new Intent(this, SettingsActivity.class));
+		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
---
+++ app/src/main/java/com/jon/buzz/activities/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.jon.buzz.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+
+	}
+}
